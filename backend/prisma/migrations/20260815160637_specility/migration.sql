1d9136a07660441b79aef485e89bc97281077a9d
Azharul Islam <azharulip@gmail.com>
2026-08-15 22:07:19 +0600

    Speciality model initilaized

---
+++ backend/prisma/migrations/20260815160637_specility/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "speciality" (
+    "id" TEXT NOT NULL,
+    "icon" TEXT,
+    "title" TEXT NOT NULL,
+    "description" VARCHAR(250),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "speciality_pkey" PRIMARY KEY ("id")
+);
