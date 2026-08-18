cdb89a1c0d9ff71031abaf4471a3b13b193004f4
Azharul Islam <azharulip@gmail.com>
2026-08-18 12:46:20 +0600

    auth guard added and applied it to specility routes

--- backend/src/modules/speciality/speciality.route.ts
+++ backend/src/modules/speciality/speciality.route.ts
@@ -2,14 +2,16 @@
 
 import express from 'express';
 import { specialityController } from './speciality.controller';
+import { checkAuth } from '../../middlewares/checkAuth';
+import { Role } from '../../genereted/prisma/enums';
 
 
 const router = express.Router();
 
 router.post("/create", specialityController.createSpecialities)
-router.get("/all", specialityController.getAllSpeciality)
+router.get("/all", checkAuth(Role.ADMIN, Role.SUPER_ADMIN, Role.DOCTOR), specialityController.getAllSpeciality)
 router.put("/update/:id", specialityController.updateSpeciality)
 router.delete("/delete/:id", specialityController.deleteSpeciality)
 router.get("/single/:id", specialityController.getSingleSpeciality)
 
-export const specialityRouter= router
+export const specialityRouter = router
